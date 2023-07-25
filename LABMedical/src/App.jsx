f6320b3a41780f0dc61f3c97e0e80e381483027f
Lucas Baffi <lucasrenatobaffi@hotmail.com>
2023-07-25 04:22:11 -0300

    ajustes sidebar e tollbar

--- LABMedical/src/App.jsx
+++ LABMedical/src/App.jsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-=======
->>>>>>> aa5aabb3a788ce133ecc97062e5e865f1c226313
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-<<<<<<< HEAD
+
   Navigate,
 } from 'react-router-dom';
 import Login from './pages/Login/Login';
@@ -55,50 +52,8 @@ function App() {
 }
 
 export default App;
-=======
-  Navigate
-} from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Home from './pages/Home/Home.jsx';
-// import Register from './pages/Register/Register';
-import './App.css'
-import { useContext } from 'react';
-import { LoginContext } from './Context/LoginContext';
-import ToolBar from './Compenents/OtherComponents/ToolBar/ToolBar'
-import SideBar from './Compenents/OtherComponents/SideBar/SideBar';
 
-
-
-function App() {
-
-  const { loginOn } = useContext(LoginContext)
-
-
-
-  return (
-
-    <Router>
-
-
-
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route
-          path="/"
-          element={loginOn ? <Navigate to="/home" /> : <Login />}
-        />
-      </Routes>
-
-
-    </Router>
-
-  )
-}
-
-export default App
 
 
  
->>>>>>> aa5aabb3a788ce133ecc97062e5e865f1c226313
+
